2cf031c9986015dff6dca00a5eef008dbeda70a9
MetalCRT <metalcraftx@gmail.com>
2023-03-30 14:49:41 -0300

    Add Colored Directed Arrow Sprites

--- Scripts/M_ArrowUp.gd
+++ Scripts/M_ArrowUp.gd
@@ -9,3 +9,11 @@ func _process(delta):
 	if position.y <= -50:
 		queue_free()
 		print("Adios")
+
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	pass # Replace with function body.
+
+
+func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	pass # Replace with function body.
