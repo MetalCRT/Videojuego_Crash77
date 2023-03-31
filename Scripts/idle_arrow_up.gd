6b0a73b8d29d19182f0bf4a80b5ed32cd1188c4c
MetalCRT <metalcraftx@gmail.com>
2023-03-31 11:35:41 -0300

    Add Logic to hit Arrows with receptor

---
+++ Scripts/idle_arrow_up.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@onready var anim = $AnimationPlayer
+var sensor = 0
+
+func _process(delta):
+	#Hit Note
+	if sensor == 1:
+		if Input.is_action_just_pressed("ui_up"):
+			anim.play("Hit")
+	
+	#Miss Note
+	if sensor == 0:
+		if Input.is_action_just_pressed("ui_up"):
+			anim.play("Miss")
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	sensor = 1
+	Global.sensor_ArrowUp = 1
+
+
+
+func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	sensor = 0
+	Global.sensor_ArrowUp = 0
