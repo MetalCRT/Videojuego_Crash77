7f7cf91a02397bd61bc904e1f7baa251f8085086
MetalCRT <metalcraftx@gmail.com>
2023-04-05 18:01:39 -0400

    Add arrow generators to main scene

---
+++ Scripts/main_scene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+const AL = preload("res://Scenes/m_arrow_left.tscn")
+const AD = preload("res://Scenes/m_arrow_down.tscn")
+const AU = preload("res://Scenes/m_arrow_up.tscn")
+const AR = preload("res://Scenes/m_arrow_right.tscn")
+
+var random = 0
+var RNG = RandomNumberGenerator.new()
+
+func _process(delta):
+	pass
