732957cd6389d6fae6db90a309b2fdfbbfec7463
MetalCRT <metalcraftx@gmail.com>
2023-04-05 18:14:35 -0400

    Add randomizer arrow generator on main scene

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -10,3 +10,28 @@ var RNG = RandomNumberGenerator.new()
 
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	$Timer.start()
+	RNG.randomize()
+	var random_int = RNG.randi_range(0,4)
+	random = random_int
+	
+	match random :
+		1:
+			var al = AL.instantiate()
+			get_parent().add_child(al)
+			al.position = $Creator/Marker2D_AL.global_position
+		2:
+			var ad = AD.instantiate()
+			get_parent().add_child(ad)
+			ad.position = $Creator/Marker2D_AD.global_position
+		3:
+			var au = AU.instantiate()
+			get_parent().add_child(au)
+			au.position = $Creator/Marker2D_AU.global_position
+		4:
+			var ar = AR.instantiate()
+			get_parent().add_child(ar)
+			ar.position = $Creator/Marker2D_AR.global_position
