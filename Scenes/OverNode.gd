f9445a17ed7116041b02d68598100e21fd5cd3ee
danny_gon <dannygonzalez1327@hotmail.com>
2023-05-20 17:34:48 -0400

    Game Over
    ahora si

--- Scenes/OverNode.gd
+++ Scenes/OverNode.gd
@@ -9,7 +9,7 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		# Realizar acciones de game over aquí
-		get_tree().change_scene("res://Scenes/Gameover.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Gameover.tscn")
 
 
 
