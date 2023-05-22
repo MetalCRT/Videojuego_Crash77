4b12de238e5dbdcacd79007955f59c8aa685caf5
danny_gon <dannygonzalez1327@hotmail.com>
2023-05-22 16:24:40 -0400

    arreglo pa la build

--- Scenes/OverNode.gd
+++ Scenes/OverNode.gd
@@ -3,13 +3,10 @@ extends Area2D
 @onready var punto_final = $"."
 @onready var collision_shape_2d = %CollisionShape2D
 
-func _ready():
-	pass 
-	
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		# Realizar acciones de game over aquí
-		get_tree().change_scene_to_file("res://Scenes/Gameover.tscn")
+		get_tree().change_scene_to_file("res://Scenes/GameOver.tscn")
 
 
 
