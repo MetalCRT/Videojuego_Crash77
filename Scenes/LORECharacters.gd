c86d262f951fd20dfb53e6e2c2069ea5b21f7565
MarioMontgomery1 <mario.jimenez.a@ug.uchile.cl>
2023-07-08 01:05:36 -0400

    lore personajes y tutorial

--- Scenes/LORECharacters.gd
+++ Scenes/LORECharacters.gd
@@ -1,11 +1,39 @@
 extends MarginContainer
-
+@onready var buttonback = %buttonback
+@onready var gallardoo = %Gallardoo
+@onready var tynicc = %Tynicc
+@onready var tomyy = %Tomyy
+@onready var parlantinn = %Parlantinn
+@onready var loree = %Loree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.OFICIAL
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	buttonback.pressed.connect(_on_buttonback_pressed)
+	gallardoo.pressed.connect(_on_gallardoo_pressed)
+	tynicc.pressed.connect(_on_tynicc_pressed)
+	tomyy.pressed.connect(_on_tomyy_pressed)
+	parlantinn.pressed.connect(_on_parlantinn_pressed)
+	
+func _on_buttonback_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	
+func _on_gallardoo_pressed():
+	loree.show_text(tr("GALLARDO"))
+	update_rich_text(tr("GALLARDO"))
+	
+func _on_tynicc_pressed():
+	loree.show_text(tr("TYNIC"))
+	update_rich_text(tr("TYNIC"))
+	
+func _on_tomyy_pressed():
+	loree.show_text(tr("TOMMY"))
+	update_rich_text(tr("TOMMY"))
+	
+func _on_parlantinn_pressed():
+	loree.show_text(tr("GALLARDO"))
+	update_rich_text(tr("GALLARDO"))
+	
+func update_rich_text(text: String):
+	loree.bbcode_text = text
+	
+	
